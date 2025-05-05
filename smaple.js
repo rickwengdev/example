b096a6384eb69fba2127ac4c6b002b82800cf4c3
rickwengdev <rick783256@proton.me>
2025-05-05 08:27:11 +0800

    建立基本的 JavaScript 文件設置

---
+++ smaple.js
@@ -0,0 +1,10 @@
+// sample.js
+// Basic setup for a JavaScript file
+
+// Function to log a message
+function setup() {
+    console.log("Setup complete!");
+}
+
+// Call the setup function
+setup();
